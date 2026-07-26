618fed4f6b14a6e79955792852098c8580db30e6
duoduo <2326064185@qq.com>
2026-07-26 08:20:26 +0000

    P5703 【深基2.例5】苹果采购

---
+++ src/P5703.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int student_number, apple_number;
+    cin >> student_number >> apple_number;
+    cout << student_number * apple_number << endl;
+    return 0;
+}
