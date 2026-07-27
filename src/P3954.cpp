4a8ff94e1db76c44a65ed8c514ea980e3853cd2d
duoduo <2326064185@qq.com>
2026-07-27 09:34:40 +0000

    P3954 [NOIP 2017 普及组] 成绩

---
+++ src/P3954.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    std::cout << a/10*2 + b/10*3 + c/10*5 << std::endl;
+    return 0;
+}
